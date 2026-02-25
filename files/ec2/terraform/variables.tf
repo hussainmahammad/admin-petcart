3ed2eadc3f2fdc8cacaa1bb4d8f51e270a2d37b6
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-02-25 11:42:01 +0530

    Update variables.tf

--- files/ec2/terraform/variables.tf
+++ files/ec2/terraform/variables.tf
@@ -10,7 +10,7 @@ variable "region" {
 variable "project_name" {
   description = "Project name prefix"
   type        = string
-  default     = "weblancehub"
+  default     = "admin-petcart"
 }
 
 # ----------------------------
